666f6c326a68f834e441b618968f8d18c781b325
shushant80 <79268671+shushant80@users.noreply.github.com>
2024-11-12 02:19:55 +0530

    Update testimonialsData.js

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,25 +1,25 @@
-import image1 from "../assets/t-image1.png";
-import image2 from "../assets/t-image2.jpg";
-import image3 from "../assets/t-image3.jpg";
+import image1 from "../assets/WhatsApp Image 2024-11-12 at 00.09.41.jpeg";
+import image2 from "../assets/1716116413440.jpg";
+// import image3 from "../assets/t-image3.jpg";
 
 export const testimonialsData = [
   {
     image: image1,
     review:
       "Quick Solutions Key transformed the way we manage our business. Its intuitive interface and powerful features have saved us countless hours each week. A game-changer!",
-    name: 'MATHEW HENDRICKSON',
-    status : 'ENTREPRENEUR'
+    name: 'Shubham Sharma',
+    status : 'Gym Trainer'
   },
   {
     image: image2,
     review: "I was amazed at how quickly we could streamline our operations with this app. It's reliable, user-friendly, and packed with features that meet all our needs. Highly recommended!",
-    name: 'JOHN KEVIN',
-    status: 'COACH'
+    name: 'Shushant',
+    status: 'Gym trainer'
   },
-  {
-    image : image3,
-    review:" Quick Solutions Key has been a lifesaver for our business. From task management to communication, it covers everything effortlessly. We can't imagine working without it anymore.",
-    name: 'FRANKLIN',
-    status: "CUSTOMER"
-  }
+  // {
+  //   image : image3,
+  //   review:" Quick Solutions Key has been a lifesaver for our business. From task management to communication, it covers everything effortlessly. We can't imagine working without it anymore.",
+  //   name: 'FRANKLIN',
+  //   status: "CUSTOMER"
+  // }
 ];
