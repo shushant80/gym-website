8e40d351bff44181e9db8cf16182b30cc1d15849
shushant80 <79268671+shushant80@users.noreply.github.com>
2024-11-12 02:22:18 +0530

    Update Plans.jsx

--- src/components/plans/Plans.jsx
+++ src/components/plans/Plans.jsx
@@ -1,45 +1,45 @@
 import React from 'react'
-import {plansData} from '../../data/plansData'
+import { plansData } from '../../data/plansData'
 import whiteTick from '../../assets/whiteTick.png'
 import './Plans.css'
 
 const Plans = () => {
   return (
     <div className="plans-container">
-        <div className="blur plans-blur-1"></div>
-        <div className="blur plans-blur-2"></div>
-        <div 
-            className="programs-header"
-            style={{gap: '2rem'}}
-        >
-            <span className="stroke-text">READY TO START</span>
-            <span>YOUR JOURNEY</span>
-            <span className="stroke-text">NOW WITH US</span>   
-        </div>
-        <div className="plans">
-            {plansData.map((plan, i) => (
-                <div className="plan" key={i}>
-                    {plan.icon}
-                    <span>{plan.name}</span>
-                    <span> {plan.price}</span>
+      <div className="blur plans-blur-1"></div>
+      <div className="blur plans-blur-2"></div>
+      <div className="programs-header" style={{ gap: '2rem' }}>
+        <span className="stroke-text">READY TO START</span>
+        <span>YOUR JOURNEY</span>
+        <span className="stroke-text">NOW WITH US</span>   
+      </div>
+      <div className="plans">
+        {plansData.map((plan, i) => (
+          <div className="plan" key={i}>
+            {plan.icon}
+            <span>{plan.name}</span>
+            <div className="price-section">
+              <span className="original-price">
+                <s>{plan.price}</s>
+              </span>
+              <span className="offer-price">{plan.offerPrice}</span>
+            </div>
 
-                    <div className="features">
-                    {plan.features.map((feature, i) => (
-                        <div className="feature">
-                            <img src={whiteTick} alt="" />
-                            <span key={i}>{feature}</span>
-                        </div>
-                    ))}
-                    </div>     
-                    {/* <div>
-                        <span>See more benefits -></span>
-                    </div>     */}
-                    <button className="btn">Join now</button>
-                </div>  
-            ))}
-        </div>
+            <div className="features">
+              {plan.features.map((feature, i) => (
+                <div className="feature" key={i}>
+                  <img src={whiteTick} alt="" />
+                  <span>{feature}</span>
+                </div>
+              ))}
+            </div>     
+            
+            <button className="btn">Join now</button>
+          </div>  
+        ))}
+      </div>
     </div>
   )
 }
 
-export default Plans
+export default Plans;
